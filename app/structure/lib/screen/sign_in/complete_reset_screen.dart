d69050c644c2b622c7a7d44c928665fdeba6d338
Choi SeungMin <sdouf5054@korea.ac.kr>
2024-07-02 17:37:37 +0900

    fix: table fix

---
+++ app/structure/lib/screen/sign_in/complete_reset_screen.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:go_router/go_router.dart';
+import 'package:structure/components/main_button.dart';
+import 'package:structure/config/pallete.dart';
+
+class CompleteResetScreen extends StatelessWidget {
+  const CompleteResetScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Column(
+        children: [
+          Expanded(
+            child: Stack(
+              children: [
+                Center(
+                  child: Transform.translate(
+                    offset: Offset(0, 96.h),
+                    child: Stack(
+                      children: [
+                        Positioned(
+                          left: 0.w,
+                          bottom: 0,
+                          child: const Icon(
+                            Icons.auto_awesome,
+                            color: Palette.starIcon,
+                          ),
+                        ),
+                        Icon(
+                          Icons.check_circle,
+                          size: 120.w,
+                          color: Palette.mainBtnAtvBg,
+                        ),
+                        const Positioned(
+                          right: 0,
+                          child: Icon(
+                            Icons.auto_awesome,
+                            color: Palette.starIcon,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(left: 46.w, top: 182.h),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('비밀번호 변경 메일이\n전송되었습니다 !', style: Palette.h2),
+                      SizedBox(
+                        height: 13.h,
+                      ),
+                      Text('이메일을 확인해주세요.', style: Palette.h5Grey),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(bottom: 40.h),
+            child: MainButton(
+              onPressed: () => context.go('/sign-in'),
+              text: '확인',
+              width: 640.w,
+              height: 96.h,
+              mode: 1,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ app/structure/lib/viewModel/my_page/delete_user_view_model.dart
@@ -0,0 +1,101 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:structure/config/labels.dart';
+import 'package:structure/config/pallete.dart';
+import 'package:structure/dataSource/remote_data_source.dart';
+import 'package:structure/model/user_model.dart';
+
+class DeleteUserViewModel with ChangeNotifier {
+  UserModel userModel;
+  DeleteUserViewModel({required this.userModel});
+
+  final formKey = GlobalKey<FormState>();
+  bool isLoading = false;
+
+  TextEditingController password = TextEditingController();
+
+  bool _isValidPw = false;
+
+  /// 기존 비밀번호 유효성 검사
+  String? pwValidate(String? value) {
+    if (value!.isEmpty) {
+      _isValidPw = false;
+      return '비밀번호를 입력하세요.';
+    } else {
+      _isValidPw = true;
+      return null;
+    }
+  }
+
+  /// 값이 올바르게 입력됐는지 확인
+  bool isValid() {
+    if (_isValidPw) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  late BuildContext _context;
+
+  /// 회원 탈퇴 함수
+  Future<void> deleteUser(BuildContext context) async {
+    _context = context;
+    isLoading = true;
+    notifyListeners();
+    try {
+      // 기존 firebase user 정보 불러오기 (현재 로그인된 유저)
+      User? user = FirebaseAuth.instance.currentUser;
+      if (user != null) {
+        // 비밀번호 변경 전 firebase에 재인증 필요
+        await FirebaseAuth.instance.currentUser?.reauthenticateWithCredential(
+          EmailAuthProvider.credential(
+            email: userModel.userId!,
+            password: password.text,
+          ),
+        );
+
+        // Firebase 유저 삭제
+        await user.delete();
+
+        // DB에서 유저 삭제 API 호출
+        final response = await RemoteDataSource.deleteUser(userModel.userId!);
+        if (response == null) {
+          throw Error();
+        }
+
+        _success();
+      } else {
+        print('User does not exist.');
+      }
+    } on FirebaseException catch (e) {
+      print('error: ${e.code}');
+      if (e.code == 'wrong-password') {
+        _showAlert(Labels.pwdNotSame); // 기존 비밀번호가 틀리면 alert 생성
+      } else {
+        _showAlert('오류가 발생했습니다.');
+      }
+    }
+    isLoading = false;
+    notifyListeners();
+  }
+
+  /// 오류 snackbar
+  void _showAlert(String message) {
+    ScaffoldMessenger.of(_context).showSnackBar(
+      SnackBar(
+        duration: const Duration(seconds: 1),
+        content: Text(message),
+        backgroundColor: Palette.alertBg,
+      ),
+    );
+  }
+
+  /// 비밀번호 변경 성공
+  void _success() {
+    password.clear();
+    _context.go('/sign-in');
+    _showAlert('회원 탈퇴가 성공적으로 처리되었습니다.');
+  }
+}
