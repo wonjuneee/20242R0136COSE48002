c645e80676373038066eeed8c3b720fdc18c9c9d
deun115 <diddmstj15@naver.com>
2024-07-16 16:12:15 +0900

    hotfix: user/update 시 전달안하는 인자 수정

--- test-backend/test-flask/api/user_api.py
+++ test-backend/test-flask/api/user_api.py
@@ -197,12 +197,14 @@ def update_user_data():
             jsonify(
                 {
                     "userId": updated_user.userId,
+                    "name": updated_user.name,
                     "homeAddr": updated_user.homeAddr,
                     "company": updated_user.company,
                     "jobTitle": updated_user.jobTitle,
                     "alarm": updated_user.alarm,
                     "type": usrType[updated_user.type],
-                    "updatedAt": updated_user.updatedAt
+                    "updatedAt": updated_user.updatedAt,
+                    "createdAt": updated_user.createdAt
                 }
             ), 
             200,
