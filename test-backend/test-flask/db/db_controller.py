3c15dd4894a3b68e9b7a151d2212d69ceac77873
{recon48} <{choe0920@naver.com}>
2024-07-24 15:52:36 +0900

    merge feature/back

--- test-backend/test-flask/db/db_controller.py
+++ test-backend/test-flask/db/db_controller.py
@@ -162,7 +162,6 @@ def create_SensoryEval(db_session, meat_data: dict, sensory_data, seqno, id, use
     item_encoder(meat_data, "seqno", seqno)
     meat_data['id'] = id
     meat_data.pop('meatId')
-    meat_data['createdAt'] = datetime.now().strftime('%Y-%m-%d')
     meat_data['period'] = calculate_period(db_session, id)
     meat_data.pop('sensoryData')
     meat_data['userId'] = userId
@@ -350,6 +349,7 @@ def create_specific_sensory_eval(db_session, s3_conn, firestore_conn, data, is_p
             user_id = safe_str(data.get("userId"))
             # sensory_eval 생성
             if any(value is not None for value in sensory_data.values()):
+                sensory_data["createdAt"] = convert2string(datetime.now(), 1)
                 new_sensory_eval = create_SensoryEval(db_session, data, sensory_data, seqno, meat_id, user_id)
                 db_session.add(new_sensory_eval)
                 db_session.commit()
@@ -383,6 +383,7 @@ def create_specific_sensory_eval(db_session, s3_conn, firestore_conn, data, is_p
                 
             # sensory_eval 생성
             if any(value is not None for value in sensory_data.values()):
+                sensory_data["createdAt"] = convert2string(existing_sensory.createdAt, 1)
                 new_sensory_eval = create_SensoryEval(db_session, data, sensory_data, seqno, meat_id, existing_user)
                 db_session.merge(new_sensory_eval)
 
@@ -418,7 +419,6 @@ def create_specific_heatedmeat_seonsory_eval(db_session, firestore_conn, s3_conn
     try:
         sensory_data = data["heatedmeatSensoryData"]
         sensory_data["filmedAt"] = data["filmedAt"]
-        sensory_data["createdAt"] = convert2string(datetime.now(), 1)
         existed_sensory_data = get_HeatedmeatSensoryEval(db_session, id, seqno)
 
         if existed_sensory_data: # 수정
@@ -431,6 +431,7 @@ def create_specific_heatedmeat_seonsory_eval(db_session, firestore_conn, s3_conn
                 meat.statusType = 0
                 db_session.merge(meat)
             sensory_data["userId"] = existed_sensory_data["userId"]
+            sensory_data["createdAt"] = existed_sensory_data["createdAt"]
             new_sensory_data = create_HeatemeatSensoryEval(sensory_data, id, seqno)
             db_session.merge(new_sensory_data)
             
@@ -438,6 +439,7 @@ def create_specific_heatedmeat_seonsory_eval(db_session, firestore_conn, s3_conn
             if not is_post: # 생성인데 PATCH 메서드
                 return ({"msg": "Heatedmeat Sensory Data Does NOT Exists", "code": 400})
             sensory_data["userId"] = data["userId"]
+            sensory_data["createdAt"] = convert2string(datetime.now(), 1)
             sensory_data["period"] = calculate_period(db_session, id)
             new_sensory_data = create_HeatemeatSensoryEval(sensory_data, id, seqno)
             db_session.add(new_sensory_data)
@@ -471,19 +473,17 @@ def create_specific_probexpt_data(db_session, data, is_post):
     
     try:
         probexpt_data = data["probexptData"]
-        probexpt_data["updatedAt"] = convert2string(datetime.now(), 1)
         existed_probexpt_data = get_ProbexptData(db_session, id, seqno, is_heated)
 
         if existed_probexpt_data: # 수정
             if is_post: # 수정이지만 POST 메서드
                 return ({"msg": "Probexpt Data Already Exists", "code": 400})
             
-            if seqno == 0 and meat.statusType == 2:
-                return ({"msg": "Already Confirmed Data", "code": 400})
-            elif seqno == 0 and meat.statusType != 2:
-                meat.statusType = 0
-                db_session.merge(meat)
+            if meat.statusType != 2:
+                return ({"msg": "Not Confirmed Data", "code": 400})
+
             probexpt_data["userId"] = existed_probexpt_data["userId"]
+            probexpt_data["updatedAt"] = existed_probexpt_data["updatedAt"]
             new_probexpt_data = create_ProbexptData(probexpt_data, id, seqno, is_heated)
             db_session.merge(new_probexpt_data)
             db_session.commit()
@@ -492,6 +492,7 @@ def create_specific_probexpt_data(db_session, data, is_post):
             if not is_post: # 생성이지만 PATCH 메서드
                 return ({"msg": "Probexpt Data Does NOT Exists", "code": 400})
             probexpt_data["userId"] = data["userId"]
+            probexpt_data["updatedAt"] = convert2string(datetime.now(), 1) # updatedAt -> createdAt으로 수정 예정
             probexpt_data["period"] = calculate_period(db_session, id)
             new_probexpt_data = create_ProbexptData(probexpt_data, id, seqno, is_heated)
             db_session.add(new_probexpt_data)
@@ -1676,6 +1677,7 @@ def get_sensory_of_meat(db_session, start, end, species, grade, is_raw):
                 .join(Meat, Meat.id == DeepAgingInfo.id)
                 .join(CategoryInfo, CategoryInfo.id == Meat.categoryId)
                 .filter(
+                    Meat.createdAt.between(start, end),
                     CategoryInfo.speciesId == species,
                     Meat.statusType == 2,
                 )
@@ -1683,12 +1685,10 @@ def get_sensory_of_meat(db_session, start, end, species, grade, is_raw):
             query = (
                 query.filter(
                     SensoryEval.seqno == 0,
-                    Meat.createdAt.between(start, end),
                 ) 
                 if is_raw 
                 else query.filter(
                     SensoryEval.seqno != 0,
-                    SensoryEval.createdAt.between(start, end),
                 )
             )
             query = (
@@ -1710,17 +1710,16 @@ def get_sensory_of_meat(db_session, start, end, species, grade, is_raw):
                 .filter(
                     CategoryInfo.speciesId == species,
                     Meat.statusType == 2,
+                    Meat.createdAt.between(start, end),
                 )
             )
             uniques_query = (
                 uniques_query.filter(
                     SensoryEval.seqno == 0,
-                    Meat.createdAt.between(start, end),
                 )
                 if is_raw
                 else uniques_query.filter(
                     SensoryEval.seqno != 0,
-                    SensoryEval.createdAt.between(start, end),
                 )
             )
             uniques_query = (
@@ -1887,17 +1886,16 @@ def get_sensory_of_raw_heatedmeat(db_session, start, end, species, grade, is_raw
                 .filter(
                     CategoryInfo.speciesId == species,
                     Meat.statusType == 2,
+                    Meat.createdAt.between(start, end),
                 )
             )
             query = (
                 query.filter(
                     HeatedmeatSensoryEval.seqno == 0,
-                    Meat.createdAt.between(start, end),
                 ) 
                 if is_raw 
                 else query.filter(
                     HeatedmeatSensoryEval.seqno != 0,
-                    HeatedmeatSensoryEval.createdAt.between(start, end),
                 )
             )
             query = (
@@ -1918,17 +1916,16 @@ def get_sensory_of_raw_heatedmeat(db_session, start, end, species, grade, is_raw
                 .filter(
                     CategoryInfo.speciesId == species,
                     Meat.statusType == 2,
+                    Meat.createdAt.between(start, end),
                 )
             )
             uniques_query = (
                 uniques_query.filter(
                     HeatedmeatSensoryEval.seqno == 0,
-                    Meat.createdAt.between(start, end),
                 )
                 if is_raw
                 else uniques_query.filter(
                     HeatedmeatSensoryEval.seqno != 0,
-                    HeatedmeatSensoryEval.createdAt.between(start, end),
                 )
             )
             uniques_query = (
@@ -2141,3 +2138,30 @@ def get_probexpt_of_processed_heatedmeat(db_session, start, end):
         ] = probexpt_data_dict
 
     return result
+
+
+def get_timeseries_of_cattle_data(db_session, start, end, meat_value):
+    # 기간 설정
+    start = convert2datetime(start, 0)  # Start Time
+    end = convert2datetime(end, 0)  # End Time
+    meat_ids = db_session.query(CategoryInfo.id).filter(CategoryInfo.primalValue == meat_value)
+    meat_id_list = [val[0] for val in meat_ids]
+    
+    stats = {}
+    for i in range(5):
+        query = (
+            db_session.query(func.avg(getattr(HeatedmeatSensoryEval, 'tenderness')))
+            .join(DeepAgingInfo, (DeepAgingInfo.id == HeatedmeatSensoryEval.id) and (DeepAgingInfo.seqno == HeatedmeatSensoryEval.seqno))
+            .join(Meat, Meat.id == DeepAgingInfo.id)
+            .join(CategoryInfo, CategoryInfo.id == Meat.categoryId)
+            .filter(
+                HeatedmeatSensoryEval.seqno == i,
+                Meat.categoryId.in_(meat_id_list),
+                Meat.statusType == 2,
+                Meat.createdAt.between(start, end)
+            )
+        )
+        query = query.one()
+        stats[str(i)] = query[0] if query[0] else 0
+    
+    return stats
