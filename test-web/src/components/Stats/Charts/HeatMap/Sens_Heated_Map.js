31549ccd9b1e5d18c2d7a7e1ff2be84bcdae0f46
Choi SeungMin <sdouf5054@korea.ac.kr>
2024-07-31 23:16:39 +0900

    fix: 분포 범례 수정

--- test-web/src/components/Stats/Charts/HeatMap/Sens_Heated_Map.js
+++ test-web/src/components/Stats/Charts/HeatMap/Sens_Heated_Map.js
@@ -76,10 +76,21 @@ export default function Sens_Heated_Map({
       enabled: false,
     },
     xaxis: {
-      type: 'numeric',
-      tickAmount: 11,
-      min: 0,
-      max: 11,
+      type: 'category',
+      categories: [
+        '1 미만',
+        '1 ~ 2',
+        '2 ~ 3',
+        '3 ~ 4',
+        '4 ~ 5',
+        '5 ~ 6',
+        '6 ~ 7',
+        '7 ~ 8',
+        '8 ~ 9',
+        '9 ~ 10',
+        '10 이상',
+      ],
+      tickPlacement: 'between',
     },
     title: {
       text: '가열육 관능데이터 범위별 분포(빈도수)',
@@ -102,10 +113,23 @@ export default function Sens_Heated_Map({
       },
       x: {
         show: true,
-        formatter: function (value, { seriesIndex }) {
+        formatter: function (value, { dataPointIndex, seriesIndex }) {
+          const categories = [
+            '1 미만',
+            '1.0 ~ 2.0',
+            '2.0 ~ 3.0',
+            '3.0 ~ 4.0',
+            '4.0 ~ 5.0',
+            '5.0 ~ 6.0',
+            '6.0 ~ 7.0',
+            '7.0 ~ 8.0',
+            '8.0 ~ 9.0',
+            '9.0 ~ 10.0',
+            '10 이상',
+          ];
           const total =
             ChartSeries[seriesIndex]?.data?.reduce((a, b) => a + b, 0) || 0;
-          return `${value} ~ ${value + 1}점 [전체 ${total}개]`;
+          return `${categories[dataPointIndex]} [전체 ${total}개]`;
         },
       },
     },
