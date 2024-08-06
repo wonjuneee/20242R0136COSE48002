0fa3ee042709889f687f54bdfbeeb083fad297e7
Choi SeungMin <sdouf5054@korea.ac.kr>
2024-08-06 11:12:47 +0900

    fix: firebase-config 주석 삭제

--- test-web/src/firebase-config.js
+++ test-web/src/firebase-config.js
@@ -16,15 +16,6 @@ const firebaseConfig = {
   appId: process.env.REACT_APP_ID,
   measurementId: process.env.REACT_MEASUREMENT_ID,
 };
-// const firebaseConfig = {
-//   apiKey: `${process.env.REACT_APP_API_KEY}`,
-//   authDomain: `${process.env.REACT_AUTH_DOMAIN}`,
-//   projectId: `${process.env.REACT_PROJECT_ID}`,
-//   storageBucket: `${process.env.REACT_STORAGE_BUCKET}`,
-//   messagingSenderId: `${process.env.REACT_MESSAGING_SENDER_ID}`,
-//   appId: `${process.env.REACT_APP_ID}`,
-//   measurementId: `${process.env.REACT_MEASUREMENT_ID}`,
-// };
 const firebase = initializeApp(firebaseConfig);
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
