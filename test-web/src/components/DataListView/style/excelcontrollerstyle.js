8736fc14c379eecd356138d6ea050e64c70abdbd
{recon48} <{choe0920@naver.com}>
2024-08-12 15:22:39 +0900

    refac:excelcontroller style 외부로 이동

---
+++ test-web/src/components/DataListView/style/excelcontrollerstyle.js
@@ -0,0 +1,20 @@
+const navy = '#0F3659';
+const style = {
+    importBtnWrapper: {
+      color: navy,
+      marginRight: '10px',
+      backgroundColor: 'white',
+      border: `1px solid ${navy}`,
+      height: '35px',
+      borderRadius: '10px',
+    },
+    exportBtnWrapper: {
+      color: navy,
+      backgroundColor: 'white',
+      border: `1px solid ${navy}`,
+      height: '35px',
+      borderRadius: '10px',
+    },
+  };
+
+  export default style
