f8b278d87c3e2fee93e530d46a16e37fc5f74819
Choi SeungMin <sdouf5054@korea.ac.kr>
2024-08-20 11:56:36 +0900

    refac: route style

---
+++ test-web/src/routes/style/dataconfirmstyle.js
@@ -0,0 +1,18 @@
+const navy = '#0F3659';
+
+const style = {
+  fixed: {
+    position: 'fixed',
+    top: '95px',
+    right: '0',
+    left: '80px',
+    zIndex: 1,
+    width: 'fit-content',
+    borderRadius: '0',
+    display: 'flex',
+    justifyContent: 'space-between',
+    height: '70px',
+  },
+};
+
+export default style;
---
+++ test-web/src/routes/style/dataeditstyle.js
@@ -0,0 +1,17 @@
+const navy = '#0F3659';
+
+const style = {
+  fixed: {
+    marginTop: '80px',
+    right: '0',
+    left: '80px',
+    zIndex: 1,
+    width: 'fit-content',
+    borderRadius: '0',
+    display: 'flex',
+    justifyContent: 'space-between',
+    height: '70px',
+  },
+};
+
+export default style;
