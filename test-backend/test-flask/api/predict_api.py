4550990cc22ac17aee83ce041101d27b71fdacf1
deun115 <diddmstj15@naver.com>
2024-09-20 15:05:38 +0000

    fix: 불필요한 predict-api 삭제

--- test-backend/test-flask/api/predict_api.py
+++ test-backend/test-flask/api/predict_api.py
@@ -13,45 +13,6 @@ predict_api = Blueprint("predict_api", __name__)
 logger = logging.getLogger(__name__)
 
 
-# 특정 육류의 컬러팔레트 조회
-@predict_api.route("/color-palette", methods=["GET"])
-def create_meat_palette():
-    s3_conn = current_app.s3_conn
-    meat_id = request.args.get("meatId")
-    seqno = request.args.get("seqno")
-    
-    img = s3_conn.download_image(f"sensory_evals/{meat_id}-{seqno}.png")
-    result = display_palette_with_ratios(img)
-    return jsonify({"meatId": meat_id, "seqno": seqno, "result": result})
-
-
-# 특정 육류의 텍스쳐 정보 조회
-@predict_api.route("/texture-info", methods=["GET"])
-def create_meat_texture_info():
-    s3_conn = current_app.s3_conn
-    meat_id = request.args.get("meatId")
-    seqno = request.args.get("seqno")
-    
-    img = s3_conn.download_image(f"sensory_evals/{meat_id}-{seqno}.png")
-    texture_result = create_texture_info(img)
-    return texture_result
-
-
-# 특정 육류의 텍스쳐 정보 조회
-@predict_api.route("/lbp-gabor", methods=["GET"])
-def create_meat_lbp_garbor_images():
-    s3_conn = current_app.s3_conn
-    meat_id = request.args.get("meatId")
-    seqno = request.args.get("seqno")
-    
-    img = s3_conn.download_image(f"sensory_evals/{meat_id}-{seqno}.png")
-    
-    lbp_result = lbp_calculate(s3_conn, img, meat_id, seqno)
-    gabor_result = gabor_texture_analysis(s3_conn, img, meat_id, seqno)
-    pprint.pprint(gabor_result)
-    return lbp_result, gabor_result
-
-
 # 원육 이미지에 대해서 opencv 전처리 진행 (컬러팔레트, 텍스쳐 정보, lbp-gabor, 단면 이미지)
 @predict_api.route("/process-image", methods=["POST", "PATCH"])
 def create_raw_meat_opencv_info():
